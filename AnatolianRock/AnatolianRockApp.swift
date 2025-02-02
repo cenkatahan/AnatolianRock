eb4a581355e30eed07c591616aff48fa51c82fe8
C. Atahan Özbek <atahanozzbek@gmail.com>
2025-02-03 02:59:34 +0300

    Initial Commit

---
+++ AnatolianRock/AnatolianRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnatolianRockApp.swift
+//  AnatolianRock
+//
+//  Created by cenk atahan özbek on 3.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct AnatolianRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnatolianRock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnatolianRock
+//
+//  Created by cenk atahan özbek on 3.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
